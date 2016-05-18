fdfa84826e6fa4443a7dc41d04b3796ae0400e5f
Kiki Jewell <kikiorgg@gmail.com>
2016-05-18 12:04:04 -0700

    fix(Main: typo) Ok, so trying a new workaround.  I guess you can't iterate through a collection of Motors and treat them as Motors.  Silly.  So I iterate through my known list of keys, and key into the list of Motors.  Yeah, huh, ok?!  You try to mess with me, see how I take it!
    Was:
    Um...  Not sure why this broke.  The ingr_pumps is a collection of Motors.  Somehow, it thinks
    a loop through all of them means each one is a str.  Hmm.  Ok.  So print it and see what it is.

--- DrinkBot.py
+++ DrinkBot.py
@@ -122,8 +122,8 @@ for each_motor in range(1, 5):
     calibration_factor = drinks["Calibration"][each_ingredient]
     ingr_pumps[each_ingredient] = Motors( each_ingredient, calibration_factor )
 
-for each_pump in ingr_pumps:
-    print "Each pump: " + each_pump
+for each_pump in ingr_list:
+    print "Each pump: " + ingr_pumps[each_pump].name()
 
 
 
