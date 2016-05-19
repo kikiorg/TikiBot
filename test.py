bd2bbae1183750b1e2cf6fdd60228b3ee03fe5d8
Kiki Jewell <kikiorgg@gmail.com>
2016-05-18 19:53:06 -0700

    chore(general cleanup) Cleaning out the unneeded code and comments, in preparation for sripping the file and using it for something else. This archive is the home of all the tests I tried.

--- test.py
+++ test.py
@@ -4,53 +4,6 @@
 # Invented by Kiki Jewell with a little help from Spaceman Sam, May 6, 2016
 
 import time
-import csv
-import atexit
-
-
-
-#######################
-#     DRINK MAKER     #
-#######################
-
-############################################
-# Initialize the motors on the Bot         #
-############################################
-# Set up the address for each of the pumps #
-# NOTE: Since we don't have all 3 hats,    #
-#   I've commented out for the other       #
-#   two boards, until they come in         #
-############################################
-
-# bottom hat is default address 0x60 
-# Board 0: Address = 0x60 Offset = binary 0000 (no jumpers required)
-#bottom_hat = Adafruit_MotorHAT(addr=0x60)
-
-# middle hat has A0 jumper closed, so its address 0x61.
-# Board 1: Address = 0x61 Offset = binary 0001 (bridge A0)
-#middle_hat = Adafruit_MotorHAT(addr=0x61)
-# top hat has A0 jumper closed, so its address 0x62. 
-# Board 2: Address = 0x62 Offset = binary 0010 (bridge A1, the one above A0)
-###   top_hat = Adafruit_MotorHAT(addr=0x62)
-
-# recommended for auto-disabling motors on shutdown!
-#def turnOffMotors():
-#    bottom_hat.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-#    bottom_hat.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-#    bottom_hat.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-#    bottom_hat.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
-
-
-# middle_hat.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-# middle_hat.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-# middle_hat.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-# middle_hat.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
-# top_hat.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-# top_hat.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-# top_hat.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-# top_hat.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
-
-#atexit.register(turnOffMotors)
 
 #####################
 # Bottom Hat motors #
@@ -104,43 +57,3 @@ thread1 = Motors(ingr_pumps["one"],1)
 thread2 = Motors(ingr_pumps["two"],3)
 thread3 = Motors(ingr_pumps["three"],6)
 thread4 = Motors(ingr_pumps["four"],9)
-#thread1.start() # This actually causes the thread to run
-#thread2.start()
-#thread3.start()
-#thread4.start()
-#thread1.join()  # This waits until the thread has completed
-#thread2.join()
-#thread3.join()
-#thread4.join()
-# At this point, both threads have completed
-
-#from twisted.internet import reactor
-
-#def f(s):
-#    print "this will run 3.5 seconds after it was scheduled: %s" % s
-
-#reactor.callLater(3.5, f, "hello, world")
-
-## f() will only be called if the event loop is started.
-#reactor.run()
-
-####### Sample code to test interrupts
-    # def runEverySecond( some_text ):
-    #print some_text
-
-    #l = task.LoopingCall(runEverySecond, "Kiki a second has passed")
-    #l.start(1.0) # call every second
-
-    # l.stop() will stop the looping calls
-    #reactor.run()
-####### END Temp code to test interrupts
-
-
-
-
-#######################
-# PRINT INGREDIENTS   #
-#######################
-# This prints all the ingredients, including 'Recipe'
-
-
