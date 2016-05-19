90b0019747685f44bcf2a9ab5a0c359f7c3b94a6
Kiki Jewell <kikiorgg@gmail.com>
2016-05-18 20:57:31 -0700

    chore(test: need to initialize 8 pumps, while increment Class member during __init__) There is no calibration list anymore.  Took that out.  Handled within the Motors Class now.
    Slight typo -- broken line
    
    Small change, looping now through 8 pumps!  W00t!
    
    Another small typo: forgot to specify the Class member.
    
    Small typo -- initializing the pump to 0 since it increments before using,
    but then I *did* use it before incrementing, so instead, I used None.
    I don't really like that solution.  But will clean it up later. (Yeah right. j/k)
    
    First try at my sneaky plan!  Cross fingers!
    
    Was:
    I'm hoping to increment through all the motors as each is initialized.
    Kinda sneaky. :)

--- DrinkBot.py
+++ DrinkBot.py
@@ -151,8 +151,8 @@ while True:
         for each_ingredient in drinks[my_drink]:
             if drinks[my_drink][each_ingredient] > 0:
                 print each_ingredient + ": " + drinks[my_drink][each_ingredient]
-                print "Normalized: ", float(drinks[my_drink][each_ingredient]) * calibration_factor[each_ingredient], " seconds."
-                ingr_pumps[each_ingredient].dispense(float(drinks[my_drink][each_ingredient]) * calibration_factor[each_ingredient])
+                print "Normalized: ", float(drinks[my_drink][each_ingredient]) * ingr_pumps[each_ingredient].calibration_factor, " seconds."
+                ingr_pumps[each_ingredient].dispense(float(drinks[my_drink][each_ingredient]))
 
 # Close the file at the end.
 # Note, this annoys me and is not tidy to leave the file open the whole time!
