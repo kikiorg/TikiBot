de25774ca532145b72eff766f44255ab89976ba0
Kiki Jewell <kikiorgg@gmail.com>
2016-06-19 10:11:09 -0700

    Calibration debugged (not yet tested with liquid) Yesterday was a disaster.  Calibration was not working, and I couldn't connect my Mac, and the screen was too tiny to develop on, and it was outside in the sun.  Frustrating. Took everything home and debugged blind, but I think this works.  Will test today and push any tweeks.

--- Motors.py
+++ Motors.py
@@ -11,6 +11,7 @@ import atexit
 
 import threading
 
+
 class ThreadMe(threading.Thread):
     def __init__(self, motor, time, name):
         # I need only the motor, not the whole list for this.
@@ -20,14 +21,15 @@ class ThreadMe(threading.Thread):
         self.time = time
         self.name = name
         self.start()
-        #self.join() # Oops, this immediately stops the main thread and waits for your thread to finish
+        # self.join() # Oops, this immediately stops the main thread and waits for your thread to finish
 
     def run(self):
         self.motor.setSpeed(255)
         self.motor.run(Adafruit_MotorHAT.FORWARD)
-        # print self.name + " dispensing now."
+        print self.name + " dispensing now for", self.time, "seconds."
         time.sleep(self.time)
         self.motor.run(Adafruit_MotorHAT.RELEASE)
+
 
 #############################
 #  NOTES: PUMP CALIBRATION  #
@@ -59,6 +61,7 @@ class ThreadMe(threading.Thread):
 class LessThanZeroException(Exception):
     pass
 
+
 class HatNotConnected(Exception):
     pass
 
@@ -72,8 +75,8 @@ class HatNotConnected(Exception):
 #   two boards, until they come in         #
 ############################################
 
-#self.__modulations = (nfc.nfc_modulation * len(mods))()
-#for i in range(len(mods)):
+# self.__modulations = (nfc.nfc_modulation * len(mods))()
+# for i in range(len(mods)):
 #    self.__modulations[i].nmt = mods[i][0]
 #    self.__modulations[i].nbr = mods[i][1]
 
@@ -85,14 +88,15 @@ hat_stack.append(Adafruit_MotorHAT(addr=0x60))
 # middle hat has A0 jumper closed, so its address 0x61.
 # Board 1: Address = 0x61 Offset = binary 0001 (bridge A0)
 hat_stack.append(Adafruit_MotorHAT(addr=0x61))
-#hat_stack[1] = Adafruit_MotorHAT(addr=0x61)
+# hat_stack[1] = Adafruit_MotorHAT(addr=0x61)
 
 # top hat has A1 jumper closed, so its address 0x62.
 # Board 1: Address = 0x62 Offset = binary 0010 (bridge A1)
 hat_stack.append(Adafruit_MotorHAT(addr=0x62))
-#hat_stack[2] = Adafruit_MotorHAT(addr=0x62)
+# hat_stack[2] = Adafruit_MotorHAT(addr=0x62)
 
-all_hats = Adafruit_MotorHAT(addr=0x70) # Not used, but should address all hats at once
+all_hats = Adafruit_MotorHAT(addr=0x70)  # Not used, but should address all hats at once
+
 
 # Quick test of all motors -- this turns them all on for one second
 def test_all_motors():
@@ -104,6 +108,7 @@ def test_all_motors():
             time.sleep(1)
             hat_stack[each_hat].getMotor(each_motor).run(Adafruit_MotorHAT.RELEASE)
 
+
 # Turn off all motors -- this is registered to run at program exit: atexit.register(turnOffMotors)
 # recommended for auto-disabling motors on shutdown!
 def turnOffMotors():
@@ -113,13 +118,16 @@ def turnOffMotors():
         hat_stack[1].getMotor(each_motor).run(Adafruit_MotorHAT.RELEASE)
         hat_stack[2].getMotor(each_motor).run(Adafruit_MotorHAT.RELEASE)
         # Motors.top_hat.getMotor(each_motor).run(Adafruit_MotorHAT.RELEASE)
+
+
 # recommended for auto-disabling motors on shutdown!
 atexit.register(turnOffMotors)
+
 
 class Motors():
     # We assume these are pumps that dispense about 2oz every 60 seconds.
     peristaltic_2oz = 2
-    calibration_seconds = 10
+    calibration_seconds = 60
     # If the calibration value for a pump is 0, then this pump is not calibrated
     not_calibrated = 0.0
     # This is how long it should take to fill the pump tubing to dispense
@@ -128,7 +136,6 @@ class Motors():
     # This is used between pump startups so there's not a massive current spike
     # from all pumps starting at once.
     current_spike_stabilze = 0.1
-
 
     # Ok, this is sneaky.  We have (possibly) 3 Hats, each with 4 possible pump controllers.
     # As I create more and more ingredient pumps, I want to iterate through all the pumps available.
@@ -165,15 +172,14 @@ class Motors():
         self.name = name
         self.thread = None
         # Quick test of each motor --Kiki
-        #self.motor.run(Adafruit_MotorHAT.FORWARD)
-        #time.sleep(1)
-        #self.motor.run(Adafruit_MotorHAT.RELEASE)
+        # self.motor.run(Adafruit_MotorHAT.FORWARD)
+        # time.sleep(1)
+        # self.motor.run(Adafruit_MotorHAT.RELEASE)
 
         # If the calibration == not_calibrated, it will run a calibration
-        # Note: not-calibrated is generally 0 (will not dispense!), so don't just copy this into self!
-	print "Kiki I;m about to calibrate."
-	# Note: calibration was not always a float, so we must force it
-	# The problem arises when 0.0 == 0 is false.
+        # Note: not-calibrated is generally 0.0 (will not dispense!), so don't just copy this into self!
+        # Note: calibration was not always a float, so we must force it
+        # The problem arises when 0.0 == 0 is false.
         self.calibration = 0.0
         self.calibration = self.calibrate_pump(float(calibration))
 
@@ -183,29 +189,35 @@ class Motors():
     # then asks for the amount actually dispensed.
     # It then calculates a normalized 1oz dispense rate.
     def calibrate_pump(self, calibration):
-	print "Kiki calibration values: ", calibration, " -- ", self.not_calibrated, " is true? ", calibration == self.not_calibrated
         if calibration == self.not_calibrated:
-            yesno = raw_input("Do you want to calibrate pump for " + self.name + "?")
-            if yesno == "yes":
+            yesno = raw_input("Do you want to calibrate pump for " + self.name + "? ")
+            if yesno in ["yes", "y", "Y", "YES"]:
+                # Must assign some kind of calibration value before dispensing -- default is peristaltic_2oz
                 self.calibration = float(1.0 / Motors.peristaltic_2oz * Motors.calibration_seconds)
-                print "Kiki self.calibraion ", self.calibration
-		self.dispense(Motors.calibration_seconds)
-		self.wait_until_done()
-                new_factor = raw_input("How much liquid was delivered?")
-                print "Note: please change the value in your .csv file for " + name
-		calibration_string.append( new_factor + ",")
-                return float(1 / new_factor * Motors.calibration_seconds)
+                self.dispense(Motors.peristaltic_2oz)
+                self.wait_until_done()
+                amount_dispensed = raw_input("How much liquid was delivered? ")
+                # 1oz / actual oz = X seconds / actual dispense time
+                # solve for X -- which is the time it really should run to dispense 1oz
+                # X seconds = 1oz/actual oz * actual dispense time
+                # So now to dispense, say, 1.5oz, you multiply 1.5 * X to equal the seconds to run
+                new_factor = float(1.0 / float(amount_dispensed) * Motors.calibration_seconds)
+                print "New factor ", new_factor
+                Motors.calibration_string += str(amount_dispensed) + ","
+                return new_factor
             else:
-                print "Well...ok, but that means I'll enter a standard 2oz for this pump and it will be inaccurate!"
-                return float(1 / Motors.peristaltic_2oz * Motors.calibration_seconds)
+                print "Well...ok, but that means I'll enter a standard ", Motors.peristaltic_2oz, " for this pump and it will be inaccurate!"
+                new_factor = float(1.0 / Motors.peristaltic_2oz * Motors.calibration_seconds)
+                return new_factor
         else:
-            return float(calibration/Motors.peristaltic_2oz  * Motors.calibration_seconds)
+            new_factor = float(1.0 / calibration * Motors.calibration_seconds)
+            return new_factor
 
     # This primes the pump.  It assumes the tubing is totally empty, but also allows the user to
     # kick the pump by 1/10ths too.
     def prime(self):
         self.thread = ThreadMe(self.motor, Motors.prime_seconds, self.name)
-        self.thread.join() # Wait until pump is done before continuing
+        self.thread.join()  # Wait until pump is done before continuing
 
         answer = raw_input("More?")
         while answer == "y":
@@ -217,12 +229,15 @@ class Motors():
         # self.calibration is multiplied by the ounces to find the time to run the pump -- must be >0
         # Note: this should always be true, but being safe here!
         if self.calibration <= 0.0:
-            raise LessThanZeroException(self.name + ' - calibration:' + str(self.calibration) + ' Must be >0 for motors to run!')
+            raise LessThanZeroException(
+                self.name + ' - calibration:' + str(self.calibration) + ' Must be >0 for motors to run!')
         # The pump will have a current spike when it first starts up.
         # This delay allows that current spike to settle to operating current.
         # That way when multiple pumps start at once, there's not a massive current spike from them all.
         time.sleep(Motors.current_spike_stabilze)
         # The pumps are run as processor threads, so all pumps can run concurrently.
+        print "Kiki Disepensing for this seconds: ", ounces * self.calibration, " name: ", self.name
+        print "Kiki ounces: ", ounces , " calibration factor: ", self.calibration, " name: ", self.name
         self.thread = ThreadMe(self.motor, ounces * self.calibration, self.name)
         # print "Finished dispensing ", ounces, " of ", self.name, "."
 
@@ -232,7 +247,6 @@ class Motors():
     # pumps get started first. If you .start() then immediately .join(), then the pumps will run one after the other
     # instead of all at once.  .join() must be run for every pump *after* all the pumps have started.
     def wait_until_done(self):
-	print "Kiki Calibration string: "
-	print self.calibration_string
+        print "Kiki Calibration string: "
+        print Motors.calibration_string
         self.thread.join()
-
