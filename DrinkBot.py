6377f8419bde2c0831583b9c8b75907312a36e11
Kiki Jewell <kikiorgg@gmail.com>
2016-10-21 11:40:28 -0700

    Feature: more sound files added All the Neverwas sounds are added. Also, the new MassiveDrinks.csv file is added -- this has many kid drinks as well as almost all the adult drinks (minus Hawaiina Eye) Note that to add back the HawaiinEye, it must be hand-coded. Needs to be fixed. Lastly, you can now type in the drink file.  Again, TikiDrinks.csv won't quite work...

--- DrinkBot.py
+++ DrinkBot.py
@@ -25,8 +25,15 @@ my_yesno = yesno()
 #############################################
 # READ DRINK LIST FROM SPREADSHEET          #
 #############################################
+print "MassiveDrinks.csv"
+print "TikiDrinks_orig.csv"
+print "KidDrinks.csv"
+drink_file = raw_input("Please enter the drink file name, or [Enter] to use TikiDrinks.csv: ")
+if drink_file is "":
+    drink_file = "TikiDrinks.csv"
+
 my_recipes = DrinkRecipes("DrinkBot.py")
-my_recipes.get_recipes_from_file('TikiDrinks.csv')
+my_recipes.get_recipes_from_file(drink_file)
 my_recipes.link_to_pumps()
 
 my_drink_ID = None
@@ -35,6 +42,23 @@ my_sound_lady = SoundEffects(sound_name="sounds/Scream.wav", channel=1)
 my_sound_Howie = SoundEffects(sound_name="sounds/HowieScream.ogg", channel=1)
 my_sound_wilhelm = SoundEffects(sound_name="sounds/wilhelm.wav", channel=1)
 my_sound_drums = SoundEffects(sound_name="sounds/BoraBora.wav", channel=1, music=True, skip=1300000)
+
+# Neverwas Screams!
+my_sound_NW_Kathy1 = SoundEffects(sound_name="sounds/Neverwas/Kathy1.wav", channel=1)
+my_sound_NW_Birdbath1 = SoundEffects(sound_name="sounds/Neverwas/Birdbath1.wav", channel=1)
+my_sound_NW_Birdbath2 = SoundEffects(sound_name="sounds/Neverwas/Birdbath2.wav", channel=1)
+my_sound_NW_Birdbath3 = SoundEffects(sound_name="sounds/Neverwas/Birdbath3.wav", channel=1)
+my_sound_NW_Birdbath4 = SoundEffects(sound_name="sounds/Neverwas/Birdbath4.wav", channel=1)
+my_sound_NW_Birdbath_monkey = SoundEffects(sound_name="sounds/Neverwas/Birdbath_monkey.wav", channel=1)
+my_sound_NW_I_heard_your_prayers = SoundEffects(sound_name="sounds/Neverwas/I_heard_your_prayers.wav", channel=1)
+my_sound_NW_Sam1 = SoundEffects(sound_name="sounds/Neverwas/Sam1.wav", channel=1)
+my_sound_NW_Sam2 = SoundEffects(sound_name="sounds/Neverwas/Sam2.wav", channel=1)
+my_sound_NW_Sam3 = SoundEffects(sound_name="sounds/Neverwas/Sam3.wav", channel=1)
+my_sound_NW_Sam4 = SoundEffects(sound_name="sounds/Neverwas/Sam4.wav", channel=1)
+my_sound_NW_Victor1 = SoundEffects(sound_name="sounds/Neverwas/Victor1.wav", channel=1)
+my_sound_NW_Victor2 = SoundEffects(sound_name="sounds/Neverwas/Victor2.wav", channel=1)
+my_sound_NW_Victor3 = SoundEffects(sound_name="sounds/Neverwas/Victor3.wav", channel=1)
+my_sound_NW_Victor4 = SoundEffects(sound_name="sounds/Neverwas/Victor4.wav", channel=1)
 
 #############################################
 #     Now start polling the RFID reader     #
@@ -63,13 +87,13 @@ while True:
     # elif my_drink_ID == "04380edafe1f80":  # Charlotte's Clipper card
     # elif my_drink_ID == "045f8552334680":  # Kiki's Clipper card
     # elif my_drink_ID == "044e906a502d80":  # Sam's Clipper card
-    # elif my_drink_ID == "0496a589ba578c":  # tiny little RFID tag
-    # elif my_drink_ID == "0496a589ba60a0":  # tiny little RFID tag
+    # elif my_drink_ID == "0496a589ba578c":  # Swan - tiny little RFID tag
+    # elif my_drink_ID == "0496a589ba60a0":  # Owl -- tiny little RFID tag
     # elif my_drink_ID == "0496a589ba56ac":  # tiny little RFID tag
-    # elif my_drink_ID == "0496a589ba665a":  # tiny little RFID tag
+    # elif my_drink_ID == "0496a589ba665a":  # White Duck -- tiny little RFID tag
     # if my_drink_ID == "dc0a723b": # The sample card that came with the device
 
-    override_cards = ["dc0a723b", "0496a589ba578c", "04380edafe1f80", "045f8552334680", "044e906a502d80", "0496a589ba60a0", "0496a589ba56ac", "0496a589ba665a"]
+    override_cards = ["dc0a723b", "04380edafe1f80", "045f8552334680", "044e906a502d80", "0496a589ba56ac"]
     if my_drink_ID in override_cards: # All the little rectangular RFIDs, all the Clipper cards, and the white card
         print "OVERRIDE!  Found an override RFID tag -- going into manual mode."
         my_drink = "test"
@@ -84,90 +108,86 @@ while True:
             my_setup = Setup(my_recipes)
             my_setup.setup_menu()
 
-    elif my_drink_ID == "ecf5dea1":
-        print "Found Tall freaky lady"
-        my_sound_lady.play_sound()
-        my_sound_lady.join()
-        my_drink = "Citrus Sunset"
     elif my_drink_ID == "6ce7dea1":
         print "Found the seahorse"
-        my_sound_Howie.play_sound()
-        my_sound_Howie.join()
-        my_drink = "Passionfruit Wellness"
-    elif my_drink_ID == "1cbfdba1":
-        print "Found the BIG BUTT brown Hawaiin idol"
-        my_sound_wilhelm.play_sound()
-        my_sound_wilhelm.join()
-        my_drink = "Cool Coconut"
-    elif my_drink_ID == "8ca3dba1":
-        print "Found the tan bottle opener"
-        my_sound_Howie.play_sound()
-        my_sound_Howie.join()
-        my_drink = "Strongly Temple"
-    elif my_drink_ID == "bc7adba1":
-        print "Found the black bottle opener"
-        my_sound_wilhelm.play_sound()
-        my_sound_wilhelm.join()
-        my_drink = "Pina Co-nada"
-
-
-
-
-
-    elif my_drink_ID == "ecf5dea1":
-        print "Found Tall freaky lady"
-        my_sound_lady.play_sound()
-        my_sound_lady.join()
-        my_drink = "Pieces of Eight"
-    elif my_drink_ID == "8ca3dba1":
-        print "Found the tan bottle opener"
-        my_sound_Howie.play_sound()
-        my_sound_Howie.join()
-        my_drink = "Hurricane"
-    elif my_drink_ID == "bc7adba1":
-        print "Found the black bottle opener"
-        my_sound_wilhelm.play_sound()
-        my_sound_wilhelm.join()
-        my_drink = "Outrigger"
-    elif my_drink_ID == "1cbfdba1":
-        print "Found the BIG BUTT brown Hawaiin idol"
-        my_sound_wilhelm.play_sound()
-        my_sound_wilhelm.join()
-        my_drink = "Hawaiian Eye"
-    elif my_drink_ID == "0cd9dea1":
-        print "Found the black Hawaiin idol"
-        my_sound_Howie.play_sound()
-        my_sound_Howie.join()
-        my_drink = "Trader Vic Grog"
-    elif my_drink_ID == "bc5bdca1":
-        print "Found the tall black idol"
-        my_sound_wilhelm.play_sound()
-        my_sound_wilhelm.join()
-        my_drink = "Scorpion"
-    elif my_drink_ID == "6ce7dea1":
-        print "Found the seahorse"
-        my_sound_Howie.play_sound()
-        my_sound_Howie.join()
+        my_sound_NW_Birdbath1.play_sound()
+        my_sound_NW_Birdbath1.join()
         my_drink = "Mai Tai"
     elif my_drink_ID == "3c62dba1":
         print "Found the Lady Virgin"
-        my_sound_lady.play_sound()
-        my_sound_lady.join()
+        my_sound_NW_Kathy1.play_sound()
+        my_sound_NW_Kathy1.join()
         my_drink = "Tail-less Scorpion"
-    elif my_drink_ID == "8c97dba1":
-        print "Found Charlotte's Hula Lady"
-        my_sound_lady.play_sound()
-        my_sound_lady.join()
-        my_drink = "Moonlight"
+    elif my_drink_ID == "bc5bdca1":
+        print "Found the tall black idol"
+        my_sound_NW_Birdbath2.play_sound()
+        my_sound_NW_Birdbath2.join()
+        my_drink = "Scorpion"
+    elif my_drink_ID == "0cd9dea1":
+        print "Found the black Hawaiin idol"
+        my_sound_NW_Sam3.play_sound()
+        my_sound_NW_Sam3.join()
+        my_drink = "Trader Vic Grog"
+    elif my_drink_ID == "ecf5dea1":
+        print "Found Tall freaky lady"
+        my_sound_NW_Birdbath4.play_sound()
+        my_sound_NW_Birdbath4.join()
+        my_drink = "Pieces of Eight"
+    elif my_drink_ID == "8ca3dba1":
+        print "Found the tan bottle opener"
+        my_sound_NW_Sam1.play_sound()
+        my_sound_NW_Sam1.join()
+        my_drink = "Hurricane"
+    elif my_drink_ID == "bc7adba1":
+        print "Found the black bottle opener"
+        my_sound_NW_Sam2.play_sound()
+        my_sound_NW_Sam2.join()
+        my_drink = "Outrigger"
     elif my_drink_ID == "ac5fdba1":
         print "Found the Chief!!!"
-        my_sound_lady.play_sound()
-        my_sound_lady.join()
+        my_sound_NW_Birdbath3.play_sound()
+        my_sound_NW_Birdbath3.join()
         my_drink = "Chief Lapu Lapu"
+    # elif my_drink_ID == "1cbfdba1":
+    #    print "Found the BIG BUTT brown Hawaiin idol"
+    #    my_sound_wilhelm.play_sound()
+    #    my_sound_wilhelm.join()
+    #    my_drink = "Hawaiian Eye"
+    elif my_drink_ID == "8c97dba1":
+        print "Found Charlotte's Hula Lady"
+        my_sound_NW_Victor1.play_sound()
+        my_sound_NW_Victor1.join()
+        my_drink = "Moonlight"
+    elif my_drink_ID == "1cbfdba1":
+        print "Found the BIG BUTT brown Hawaiin idol"
+        my_sound_NW_Victor2.play_sound()
+        my_sound_NW_Victor2.join()
+        my_drink = "Citrus Sunset"
+    elif my_drink_ID == "0496a589ba578c":
+        print "Found the Swan!!!"
+        my_sound_NW_Victor3.play_sound()
+        my_sound_NW_Victor3.join()
+        my_drink = "Passionfruit Wellness"
+    elif my_drink_ID == "ace3dea1":
+        print "Found the Funky Dunky Dude!!!"
+        my_sound_NW_I_heard_your_prayers.play_sound()
+        my_sound_NW_I_heard_your_prayers.join()
+        my_drink = "Cool Coconut"
+    elif my_drink_ID == "0496a589ba60a0":
+        print "Found the Owl!!!"
+        my_sound_NW_Victor4.play_sound()
+        my_sound_NW_Victor4.join()
+        my_drink = "Pina Co-nada"
+    elif my_drink_ID == "0496a589ba665a":
+        print "Found the White Duck!!!"
+        my_sound_NW_Sam4.play_sound()
+        my_sound_NW_Sam4.join()
+        my_drink = "Pina Colada"
     elif my_drink_ID == None:
         print "Keyboard inturrupt."
         my_drink = "Exit"
     else:
+
         print "CARD NOT FOUND!!! RFID: ", my_drink_ID
         my_drink = "new card"
 
